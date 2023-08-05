45dbc8be013e8b511a52d54f56ee504d3aea2ac3
ArtemisFowl24 <tahers7@mcmaster.ca>
2023-08-05 19:03:35 -0400

    Fixed mobile bug

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
-// function([string1, string2],target id,[color1,color2])    
-consoleText(['Hello.','About Me'], 'text',['black','black']);
-
 function consoleText(words, id, colors) {
+  // The screen width threshold below which the animation will be shown
+  const screenWidthThreshold = 768;
+
+  // Check if the screen width is below the threshold
+  const isSmallScreen = window.innerWidth < screenWidthThreshold;
+
+  if (!isSmallScreen) {
+    // If the screen is not small, display nothing (an empty string)
+    const target = document.getElementById(id);
+    target.innerHTML = '';
+    return;
+  }
+
+  // If the screen is small, proceed with the animation
   if (colors === undefined) colors = ['#fff'];
   var visible = true;
   var con = document.getElementById('console');
@@ -11,17 +22,17 @@ function consoleText(words, id, colors) {
   var stringcounnt = 0;
   var target = document.getElementById(id)
   target.setAttribute('style', 'color:' + colors[0])
-  window.setInterval(function() {
-	if (stringcounnt === 2) {
-		document.getElementById('text').innerHTML = 'About Me.';
-		//hide the cursor and stop the function
-		document.getElementById('console').style.display = 'none';
-	    return;
-	}
+  window.setInterval(function () {
+    if (stringcounnt === 2) {
+      document.getElementById('text').innerHTML = 'About Me.';
+      // Hide the cursor and stop the function
+      document.getElementById('console').style.display = 'none';
+      return;
+    }
     if (letterCount === 0 && waiting === false) {
       waiting = true;
       target.innerHTML = words[0].substring(0, letterCount)
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         var usedColor = colors.shift();
         colors.push(usedColor);
         var usedWord = words.shift();
@@ -32,9 +43,9 @@ function consoleText(words, id, colors) {
         waiting = false;
       }, 1000)
     } else if (letterCount === words[0].length + 1 && waiting === false) {
-	  stringcounnt++;
+      stringcounnt++;
       waiting = true;
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         x = -1;
         letterCount += x;
         waiting = false;
@@ -44,7 +55,7 @@ function consoleText(words, id, colors) {
       letterCount += x;
     }
   }, 120)
-  window.setInterval(function() {
+  window.setInterval(function () {
     if (visible === true) {
       con.className = 'console-underscore hidden'
       visible = false;
@@ -56,6 +67,9 @@ function consoleText(words, id, colors) {
     }
   }, 400)
 }
+
+// Call the consoleText function with the appropriate parameters
+consoleText(['Hello.', 'About Me.'], 'text', ['black', 'black']);
 
 
 
